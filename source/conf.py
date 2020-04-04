d310e0ff1af8e7ca1b15c55556c48813c1b7bd33
George Babarus <george.babarus@gmail.com>
2020-04-04 20:40:49 +0300

    Create README.rst

--- source/conf.py
+++ source/conf.py
@@ -1,52 +1,121 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -*- coding: utf-8 -*-
 
-# -- Path setup --------------------------------------------------------------
+import sys
+import os
+import re
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+if not 'READTHEDOCS' in os.environ:
+    sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('./customization/'))
 
+from sphinx.locale import _
+from sphinx_rtd_theme import __version__
+import recommonmark
+from recommonmark.transform import AutoStructify
 
-# -- Project information -----------------------------------------------------
+project = u'Local development cli - LCLI'
+slug = re.sub(r'\W+', '-', project.lower())
+version = __version__
+release = __version__
+author = u'George Babarus'
+copyright = author
+language = 'en'
 
-project = 'lcli'
-copyright = '2020, George Babarus'
-author = 'George Babarus'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.httpdomain',
+    'sphinx_rtd_theme',
+    'recommonmark'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+autosectionlabel_prefix_document = True
+
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+source_parsers = {
+}
+
+exclude_patterns = [
+    'requirements.txt'
+]
+locale_dirs = ['locale/']
+gettext_compact = False
+
+master_doc = 'index'
+suppress_warnings = ['image.nonlocal_uri']
+pygments_style = 'default'
+
+intersphinx_mapping = {
+    'rtd': ('https://docs.readthedocs.io/en/latest/', None),
+    'sphinx': ('http://www.sphinx-doc.org/en/stable/', None),
+}
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': True,
+    'navigation_depth': 5,
+}
+html_theme_path = ["../.."]
+html_logo = "customization/static/logo-wordmark-light.svg"
+html_show_sourcelink = True
+
+htmlhelp_basename = slug
+
+
+latex_documents = [
+  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
+]
+
+man_pages = [
+    ('index', slug, project, [author], 1)
+]
+
+texinfo_documents = [
+  ('index', slug, project, author, slug, project, 'Miscellaneous'),
+]
+
+
+# Extensions to theme docs
+def setup(app):
+    from sphinx.domains.python import PyField
+    from sphinx.util.docfields import Field
+
+    app.add_object_type(
+        'confval',
+        'confval',
+        objname='configuration value',
+        indextemplate='pair: %s; configuration value',
+        doc_field_types=[
+            PyField(
+                'type',
+                label=_('Type'),
+                has_arg=False,
+                names=('type',),
+                bodyrolename='class'
+            ),
+            Field(
+                'default',
+                label=_('Default'),
+                has_arg=False,
+                names=('default',),
+            ),
+        ]
+    )
+    app.add_config_value('recommonmark_config', {
+        # 'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+        'enable_math': False,
+        'enable_inline_math': False,
+        'enable_eval_rst': True,
+    }, True)
+    app.add_transform(AutoStructify)
--- source/index.rst
+++ source/index.rst
@@ -1,19 +1,27 @@
-.. lcli documentation master file, created by
-   sphinx-quickstart on Sat Apr  4 18:10:08 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
-Local environment console automation
-====================================
-
+.. include:: ../README.rst
 
 .. toctree::
+   :caption: General
+   :numbered:
    :maxdepth: 2
 
+   installing
+   configuring
+   contributing
 
-Indices and tables
-==================
+.. toctree::
+   :maxdepth: 1
+   :hidden:
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   changelog
+
+.. toctree::
+    :maxdepth: 2
+    :numbered:
+    :caption: Customization
+
+    customization/structure
+    customization/plugins
+    customization/demo
+    customization/extend
